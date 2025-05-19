eb2042119881927697e2d412d73e49a8d5f2c3e2
shandyanto <moch.shandywijayanto@gmail.com>
2025-05-19 14:02:09 +0700

    fix(bugs): update login error message

--- lib/models/login_response.dart
+++ lib/models/login_response.dart
@@ -1,19 +1,29 @@
 class LoginResponse {
   final bool success;
   final String code;
-  final TokenData data;
+  final TokenData? data;
 
   LoginResponse({
     required this.success,
     required this.code,
-    required this.data,
+    this.data,
   });
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) {
+    bool successValue = json['success'] == true;
+    String codeValue = json['code']?.toString() ?? 'unknown';
+    TokenData? dataValue;
+
+    if (json['data'] != null &&
+        json['data'] is Map<String, dynamic> &&
+        json['data']['access_token'] != null) {
+      dataValue = TokenData.fromJson(json['data']);
+    }
+
     return LoginResponse(
-      success: json['success'],
-      code: json['code'],
-      data: TokenData.fromJson(json['data']),
+      success: successValue,
+      code: codeValue,
+      data: dataValue,
     );
   }
 }
@@ -24,8 +34,9 @@ class TokenData {
   TokenData({required this.accessToken});
 
   factory TokenData.fromJson(Map<String, dynamic> json) {
+    String token = json['access_token']?.toString() ?? '';
     return TokenData(
-      accessToken: json['access_token'],
+      accessToken: token,
     );
   }
 }
